607c79163a9aa9c3e57c1dd65f396654fd190435
Huy Le <huyftw@aim.com>
2022-09-25 01:59:11 +0000

    switched tmp library from sparkfun to adafruit to improve faster reading times

--- src/capstone_pkg/capstone_pkg/tmp_pub.py
+++ src/capstone_pkg/capstone_pkg/tmp_pub.py
@@ -1,38 +1,21 @@
-# getting temperature from TMP117 through I2C and RPi4
-# accessing proper addresses and sensor value conversions taken from
-# https://learn.sparkfun.com/tutorials/python-programming-tutorial-getting-started-with-the-raspberry-pi
-# in order to embedded into a ROS2 framework
-
+# getting temperature from TMP117 through I2C and RPi
+# using libraries from adafruit - https://github.com/adafruit/Adafruit_CircuitPython_TMP117
+# and embedded it into a ros2 framework
 #!/usr.bin/env python3
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
 import time
-import smbus
+import board
+import adafruit_tmp117
 
 
 class tmp_node(Node):
     def __init__(self):
         super().__init__("tmp_node")
 
-        self.i2c_ch = 1
-        self.i2c_addr = 0x48
-
-        self.reg_temp = 0x00
-        self.reg_config = 0x01
-
-        self.bus = smbus.SMBus(self.i2c_ch)
-
-        self.val = self.bus.read_i2c_block_data(self.i2c_addr, self.reg_config, 2)
-        print("old config: ", self.val)
-        self.val[1] = self.val[1] & 0b00111111
-        self.val[1] = self.val[1] | (0b11 << 6)
-
-        self.bus.write_i2c_block_data(self.i2c_addr, self.reg_config, self.val)
-
-        self.val = self.bus.read_i2c_block_data(self.i2c_addr, self.reg_config, 2)
-        print("new config: ", self.val)
-
+        self.i2c = board.I2C()
+        self.tmp_sensor = adafruit_tmp117.TMP117(self.i2c)
 
         self.tmp_publisher = self.create_publisher(
             Float32, "tmp_node", 10
@@ -42,23 +25,10 @@ class tmp_node(Node):
         self.get_logger().info("tmp117 node started")
 
 
-    def read_temp(self):
-
-        val = self.bus.read_i2c_block_data(self.i2c_addr, self.reg_temp, 2)
-        temp_c = (val[0] << 4) | (val[1] >> 4)
-
-        bits = 12
-        if (temp_c & (1 << (bits - 1))) != 0:
-            temp_c -= (1 << bits)
-
-        temp_c *= 0.0625
-
-        return temp_c
-
-
     def callback_temp_publisher(self):
 
-        temp_c = self.read_temp()
+        # temperature sensor is not calibrated - not necessary
+        temp_c = self.tmp_sensor.temperature
         temp_f = temp_c * 1.8 + 32
 
         temp = Float32()
